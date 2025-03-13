4f6aa65b69551736772854d1a4802fcffb80752f
yhjin0704 <yhjin07@naver.com>
2025-03-13 19:19:18 +0900

    [feat] Rock 추가

--- Assets/Script/Resource/Rock.cs
+++ Assets/Script/Resource/Rock.cs
@@ -1,109 +1,122 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DropResource;
 
-public class Rock : MonoBehaviour
+public class Rock : MonoBehaviour, IBreakableObject, IRespawnable
 {
-    //public float maxHp = 100f;
-    //public float currentHp;
+    public float maxHp = 100f;
+    public float currentHp;
 
-    //public float respawnTime = 5f;
+    public float respawnTime = 5f;
 
-    //public DropItem[] dropItems;
+    public DropItem[] dropItems;
 
-    //private Collider collider_;
-    //private Renderer[] renderer_;
+    private Collider collider_;
+    private Renderer[] renderer_;
+    private GameObject debrisRenderer;
+    private GameObject debrisPrefab;
 
-    //private bool isBreak = false;
+    private bool isBreak = false;
 
-    //void Awake()
-    //{
-    //    currentHp = maxHp;
+    void Awake()
+    {
+        currentHp = maxHp;
 
-    //    collider_ = GetComponentInChildren<Collider>();
-    //    renderer_ = GetComponentsInChildren<Renderer>();
+        collider_ = GetComponentInChildren<Collider>();
+        renderer_ = GetComponentsInChildren<Renderer>();
 
-    //    if (dropItems == null || dropItems.Length == 0)
-    //    {
-    //        dropItems = new DropItem[2];
+        if (dropItems == null || dropItems.Length == 0)
+        {
+            dropItems = new DropItem[3];
 
-    //        dropItems[0] = new DropItem();
-    //        dropItems[0].dropItemPrefab = Resources.Load<GameObject>("Prefabs/Tree/Branch");
-    //        dropItems[0].minDropCount = 0;
-    //        dropItems[0].maxDropCount = 3;
-    //    }
+            dropItems[0] = new DropItem();
+            dropItems[0].dropItemPrefab = Resources.Load<GameObject>("Prefabs/Rock/Flint");
+            dropItems[0].minDropCount = 0;
+            dropItems[0].maxDropCount = 2;
 
-    //    stumpPrefab = Resources.Load<GameObject>("Prefabs/Tree/TreeStump");
-    //}
+            dropItems[1] = new DropItem();
+            dropItems[1].dropItemPrefab = Resources.Load<GameObject>("Prefabs/Rock/Nitre");
+            dropItems[1].minDropCount = 0;
+            dropItems[1].maxDropCount = 1;
 
-    //public void TakeDamage(float _damage)
-    //{
-    //    if (isBreak)
-    //        return;
+            dropItems[2] = new DropItem();
+            dropItems[2].dropItemPrefab = Resources.Load<GameObject>("Prefabs/Rock/ResourceRock");
+            dropItems[2].minDropCount = 2;
+            dropItems[2].maxDropCount = 3;
+        }
 
-    //    currentHp -= _damage;
+        debrisPrefab = Resources.Load<GameObject>("Prefabs/Rock/RockDebris");
+    }
 
-    //    if (currentHp <= 0)
-    //    {
-    //        Break();
-    //    }
-    //}
+    public void TakeDamage(float _damage)
+    {
+        if (isBreak)
+            return;
 
-    //void Break()
-    //{
-    //    isBreak = true;
+        currentHp -= _damage;
 
-    //    if (dropItems != null)
-    //    {
-    //        for (int i = 0; i < dropItems.Length; i++)
-    //        {
-    //            int dropCount = Random.Range(dropItems[i].minDropCount, dropItems[i].maxDropCount + 1);
-    //            for (int j = 0; j < dropCount; j++)
-    //            {
-    //                Vector3 dropPosition = new Vector3(
-    //                    transform.position.x + Random.Range(-1f, 1f),
-    //                    transform.position.y + 1,
-    //                    transform.position.z + Random.Range(-1f, 1f)
-    //                );
-    //                Instantiate(dropItems[i].dropItemPrefab, dropPosition, Quaternion.identity);
-    //            }
-    //        }
-    //    }
+        if (currentHp <= 0)
+        {
+            Break();
+        }
+    }
 
-    //    if (collider_ != null)
-    //        collider_.enabled = false;
+    public void Break()
+    {
+        isBreak = true;
 
-    //    foreach (Renderer rd in renderer_)
-    //    {
-    //        rd.enabled = false;
-    //    }
+        if (dropItems != null)
+        {
+            for (int i = 0; i < dropItems.Length; i++)
+            {
+                int dropCount = Random.Range(dropItems[i].minDropCount, dropItems[i].maxDropCount + 1);
+                for (int j = 0; j < dropCount; j++)
+                {
+                    Vector3 dropPosition = new Vector3(
+                        transform.position.x + Random.Range(-1f, 1f),
+                        transform.position.y + 1,
+                        transform.position.z + Random.Range(-1f, 1f)
+                    );
+                    Instantiate(dropItems[i].dropItemPrefab, dropPosition, Quaternion.identity);
+                }
+            }
+        }
 
-    //    if (stumpPrefab != null)
-    //    {
-    //        stumpRenderer = Instantiate(stumpPrefab, transform.position, transform.rotation, transform);
-    //    }
+        if (collider_ != null)
+            collider_.enabled = false;
 
-    //    StartCoroutine(CRespawnCoroutine());
-    //}
+        foreach (Renderer rd in renderer_)
+        {
+            rd.enabled = false;
+        }
 
-    //IEnumerator CRespawnCoroutine()
-    //{
-    //    yield return new WaitForSeconds(respawnTime);
+        if (debrisPrefab != null)
+        {
+            debrisRenderer = Instantiate(debrisPrefab, transform.position, transform.rotation, transform);
+        }
 
-    //    currentHp = maxHp;
-    //    isBreak = false;
+        StartCoroutine(CRespawnCoroutine(respawnTime));
+    }
 
-    //    if (collider_ != null)
-    //        collider_.enabled = true;
+    public IEnumerator CRespawnCoroutine(float _respawnTime)
+    {
+        yield return new WaitForSeconds(_respawnTime);
 
-    //    foreach (Renderer rd in renderer_)
-    //    {
-    //        rd.enabled = true;
-    //    }
+        currentHp = maxHp;
+        isBreak = false;
 
-    //    if (stumpRenderer != null)
-    //    {
-    //        Destroy(stumpRenderer);
-    //    }
-    //}
+        if (collider_ != null)
+            collider_.enabled = true;
+
+        foreach (Renderer rd in renderer_)
+        {
+            rd.enabled = true;
+        }
+
+        if (debrisRenderer != null)
+        {
+            Destroy(debrisRenderer);
+        }
+    }
 }
