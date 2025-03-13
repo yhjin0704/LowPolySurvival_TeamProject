810ccd145efaf1fb6fd4db353636d26b08ba24ac
MyaMya0709 <chlehdgh96@gmail.com>
2025-03-13 10:28:57 +0900

    [Feat] Player 수정

--- Assets/Script/Player/Player.cs
+++ Assets/Script/Player/Player.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public PlayerController controller;
+    public PlayerCondition condition;
 
     private void Awake()
     {
         PlayerManager.Instance.Player = this;
         controller = GetComponent<PlayerController>();
+        condition = GetComponent<PlayerCondition>();
     }
 }
