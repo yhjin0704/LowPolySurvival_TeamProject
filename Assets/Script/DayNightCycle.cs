ad2427eff11e1cd8a7c709f9e09a41e6b7663431
000SIN <126735143+000SIN@users.noreply.github.com>
2025-03-13 16:58:09 +0900

    [Feat] 낮과 밤 기능 추가(2)
    Script 완성, Intensity 그래프 설정 완료 등

--- Assets/Script/DayNightCycle.cs
+++ Assets/Script/DayNightCycle.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class DayNightCycle : MonoBehaviour
@@ -9,21 +10,55 @@ public class DayNightCycle : MonoBehaviour
     public float fullDayLength;
     public float startTime = 0.4f; //0.5f ÀÏ¶§ Á¤¿À
     private float timeRate;
-    public Vector3 moon; //Vector 90 0 0
+    public Vector3 noon; //Vector 90 0 0
 
-    [Header('Sun')]
+    [Header("Sun")]
+    public Light sun;
+    public Gradient sunColor;
+    public AnimationCurve sunIntensity;//¹à±â º¯È­
 
-    [Header('Moon')]
+    [Header("Moon")]
+    public Light moon;
+    public Gradient moonColor;
+    public AnimationCurve moonIntensity;//¹à±â º¯È­
 
-    [Header('Other Lighting')]
+    [Header("Other Lighting")]
+    public AnimationCurve lightingIntensityMultiplier;//º¸Á¤¿ë °î¼±
+    public AnimationCurve reflectionIntensityMultiplier;//¹Ý»ç±¤ °­µµ º¯È­ °î¼±
     void Start()
     {
-        
+        timeRate = 1.0f / fullDayLength;
+        time = startTime;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        time = (time + timeRate * Time.deltaTime) % 1.0f;
+
+        UpdateLighting(sun, sunColor, sunIntensity);
+        UpdateLighting(moon, moonColor, moonIntensity);
+
+        RenderSettings.ambientIntensity = lightingIntensityMultiplier.Evaluate(time);
+        RenderSettings.reflectionIntensity = reflectionIntensityMultiplier.Evaluate(time);
+    }
+
+    void UpdateLighting(Light lightSource, Gradient gradient, AnimationCurve intensityCurve)
+    {
+        float intensity = intensityCurve.Evaluate(time);
+
+        lightSource.transform.eulerAngles = (time - (lightSource == sun ? 0.25f : 0.75f)) * noon * 4f;
+        lightSource.color = gradient.Evaluate(time);
+        lightSource.intensity = intensity;
+
+        GameObject go = lightSource.gameObject;
+        if(lightSource.intensity == 0 && go.activeInHierarchy)
+        {
+            go.SetActive(false);
+        }
+        else if(lightSource.intensity > 0 && !go.activeInHierarchy)
+        {
+            go.SetActive(true);
+        }
     }
 }
