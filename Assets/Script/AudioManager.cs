35b905644e6ec09949ffd9a0e971cc301dec91bd
bnm03090805 <bnm03090805@gmail.com>
2025-03-18 22:25:28 +0900

    [feat] 오디오 매니저 작업

---
+++ Assets/Script/AudioManager.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance;
+
+    private AudioSource bgm;
+    public AudioClip bgmClip;
+    private List<AudioSource> sfxList;
+    public AudioClip[] footstepClips;
+
+    private float masterVol;
+    private float bgmVol;
+    private float sfxVol;
+
+    public float MasterVol => masterVol;
+    public float BgmVol => bgmVol;
+    public float SFXVol => sfxVol;
+    public List<AudioSource> SFXList => sfxList;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        masterVol = 0.8f;
+        bgmVol = 0.7f; 
+        sfxVol = 0.8f;
+
+        bgm = GetComponent<AudioSource>();
+        bgm.clip = bgmClip;
+        bgm.volume = bgmVol * masterVol;
+
+        sfxList = new List<AudioSource>();
+        sfxList.Add(new AudioSource());
+    }
+
+    private void Start()
+    {
+        bgm.Play();
+    }
+
+    public void ChangeMasterVolume(float figure)
+    {
+        masterVol = figure;
+        bgm.volume = bgmVol * masterVol;
+        for (int i = 0; i < sfxList.Count; i++)
+        {
+            sfxList[i].volume = sfxVol * masterVol;
+        }
+    }
+
+    public void ChangeBGMVolume(float figure)
+    {
+        bgmVol = figure;
+        bgm.volume = bgmVol * masterVol;
+    }
+
+    public void ChangeSFXVolume(float figure)
+    {
+        sfxVol = figure;
+        for (int i = 0; i < sfxList.Count; i++)
+        {
+            sfxList[i].volume = sfxVol * masterVol;
+        }
+    }
+
+    public void PlaySFX(int id)
+    {
+        SFXList[id].Play();
+    }
+
+    public void PlayFootSteps(AudioClip[] clips)
+    {
+        sfxList[0].PlayOneShot(clips[Random.Range(0, clips.Length)]);
+    }
+
+}
---
+++ Assets/Script/Player/Footsteps.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Footsteps : MonoBehaviour
+{
+    
+    private Rigidbody _rigidbody;
+    public float footstepThreshold;
+    public float footstepRate;
+    private float footStepTime;
+
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if (Mathf.Abs(_rigidbody.velocity.y) < 0.1f)
+        {
+            if (_rigidbody.velocity.magnitude > footstepThreshold)
+            {
+                if (Time.time - footStepTime > footstepRate)
+                {
+                    footStepTime = Time.time;
+                    AudioManager.Instance.PlaySFX(0);
+                }
+            }
+        }
+    }
+}
