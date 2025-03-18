41fc91720116b2bb5f140396c925c1e2834efb40
bnm03090805 <bnm03090805@gmail.com>
2025-03-18 10:17:46 +0900

    [chore] 장비상태관련 스크립트 하나로 합침

--- Assets/Script/Player/PlayerEquipState.cs
+++ Assets/Script/Player/PlayerEquipState.cs
@@ -1,7 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
+
+public class PlayerPunch : PlayerEquipState
+{
+    private float punchDamage = 5f;
+    private float punchStamina = 10f;
+    public void ChangeController(ItemData data)
+    {
+        return;
+    }
+
+    public void ChangeController()
+    {
+        PlayerManager.Instance.Player.controller.ChangePunchAnimator();
+        PlayerManager.Instance.Player.controller.SetDamage(punchDamage);
+        PlayerManager.Instance.Player.controller.SetAttackStamina(punchStamina);
+    }
+}
 
 public class PlayerSword : PlayerEquipState
 {
