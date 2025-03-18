3dbead225520f9336400fbf747b74bc3a15ca902
000SIN <126735143+000SIN@users.noreply.github.com>
2025-03-18 10:36:14 +0900

    [Feat] Test용 코드 추가
    ReSourceManager에 시작부터 나무와 돌을 가지고 가는 코드를 넣어 집이 생성되는지 확인해 볼 수 있도록 하였다.(주석처리해놓음)

--- Assets/Script/building/ReSourceManager.cs
+++ Assets/Script/building/ReSourceManager.cs
@@ -7,6 +7,13 @@ public class ReSourceManager : MonoBehaviour
     public int wood = 0;
     public int rock = 0;  // stone → rock
 
+    //void Start()
+    //{
+    //    AddWood(20);
+    //    AddRock(20);
+    //    Debug.Log($"[초기 자원] wood: {wood}, rock: {rock}");
+    //}
+
     public void AddWood(int amount)
     {
         wood += amount;
